ae040ae2160530461862cdae5adb4e9ecab3e4c5
Dan Brubaker <danbru@gmail.com>
2019-04-23 10:16:18 -0500

    Bug squashing

--- includes/load-includes.php
+++ includes/load-includes.php
@@ -10,7 +10,7 @@
 require_once __DIR__ . '/setup.php';
 require_once __DIR__ . '/helpers.php';
 require_once __DIR__ . '/partials/index.php';
-require_once __DIR__ . '/wordpress/posts-rss-feed.php';
+// require_once __DIR__ . '/wordpress/posts-rss-feed.php';
 require_once __DIR__ . '/event-organiser/general.php';
 require_once __DIR__ . '/event-organiser/google-maps.php';
 require_once __DIR__ . '/event-organiser/calendar-feed.php';
--- includes/wordpress/posts-rss-feed.php
+++ includes/wordpress/posts-rss-feed.php
@@ -7,10 +7,17 @@
  * @since        1.4.0
  */
 
+add_filter(
+	'rss2_ns',
+	function() {
+		echo 'xmlns:media="http://search.yahoo.com/mrss/"';
+	}
+);
+
 add_action( 'rss2_item', 'bdset_add_rss_featured_image' );
 function bdset_add_rss_featured_image() {
 	if ( has_post_thumbnail() ) {
-		echo '<media:content url="' . get_the_post_thumbnail_url() . '" medium="image" width="400" />';
+		echo '<media:content url="' . get_the_post_thumbnail_url( 'medium' ) . '" medium="image" width="400" />';
 	} else {
 		return;
 	}
