faa54f0da3bb01cc2f66f720308ae6a7e5883785
Dan Brubaker <danbru@gmail.com>
2019-04-23 13:42:50 -0500

    Testing...

--- includes/wordpress/posts-rss-feed.php
+++ includes/wordpress/posts-rss-feed.php
@@ -14,7 +14,16 @@ add_filter(
 	}
 );
 
-add_action( 'rss2_item', 'bdset_add_rss_featured_image' );
+// add_action( 'rss2_item', 'bdset_add_rss_featured_image' );
+// function bdset_add_rss_featured_image() {
+// 	if ( has_post_thumbnail() ) {
+// 		echo '<media:content url="' . get_the_post_thumbnail_url( 'medium' ) . '" medium="image" width="400" />';
+// 	} else {
+// 		return;
+// 	}
+// }
+
+add_action( 'the_content_feed', 'bdset_add_rss_featured_image' );
 function bdset_add_rss_featured_image() {
 	if ( has_post_thumbnail() ) {
 		echo '<media:content url="' . get_the_post_thumbnail_url( 'medium' ) . '" medium="image" width="400" />';
